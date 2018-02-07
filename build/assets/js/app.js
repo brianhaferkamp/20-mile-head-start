e355e5ec46d1e26c9ac450acc3d307d299d5f17f
Brian Haferkamp <brian.haferkamp@gmail.com>
2018-02-07 13:07:27 -0600

    Initial commit of all project files

---
+++ build/assets/js/app.js
@@ -0,0 +1,9 @@
+// This build system utilizes the jQuery library for javascript.
+// Don't want to use jQuery? Just don't load it on the main pages
+// You can also erase this and start writing vanilla JS
+
+$(document).ready(function() {
+
+  // Write your jQuery here
+
+});
