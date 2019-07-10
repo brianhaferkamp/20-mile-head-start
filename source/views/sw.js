9c66ddc0cf8a0acf909e74a1a22cf5b6a69675be
Brian Haferkamp <brian.haferkamp@gmail.com>
2019-07-10 13:19:25 -0500

    Created service worker template

---
+++ source/views/sw.js
@@ -0,0 +1,83 @@
+//-----------------------------------------
+// Service Worker
+//
+// * Offline first
+// * Clears old cache on version change
+//
+//-----------------------------------------
+
+// Init cache name/version
+const cacheName = 'v1';
+
+// which pages/assets do you want to cache?
+const cacheAssets = [
+  'index.html',
+  'assets/css/main.css',
+  'assets/js/app.js',
+]
+
+// Install service worker
+self.addEventListener('install', (e) => {
+  console.log('Service Worker: Installed');
+  e.waitUntil(
+    caches
+      .open(cacheName)
+      .then(cache => {
+        console.log('Service Worker: Caching Files');
+        cache.addAll(cacheAssets);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+
+// Activate the service worker
+self.addEventListener('activate', (e) => {
+  console.log('Service Worker: Activated');
+
+  // Remove old caches
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        // look at all the cacheNames
+        cacheNames.map(cache => {
+          // if the current cache !== cacheName then delete it
+          if (cache !== cacheName) {
+            console.log('Service Worker: Clearing Old Cache');
+            return caches.delete(cache);
+          }
+        })
+      )
+    })
+  );
+
+});
+
+
+
+// listen for fetch event (HTTP request)
+self.addEventListener('fetch', (e) => {
+  console.log('Service Worker: Fetching');
+
+  // Offline backup
+  // e.respondWith(
+  //   // if the user is online, perform a regular HTTP request
+  //   fetch(e.request)
+  //   // if the HTTP request fails (offline) then serve the assets requested from the cache
+  //   .catch(() => caches.match(e.request))
+  // )
+
+  // Offline first
+  e.respondWith(
+    // are the files requested in the cache already?
+    caches.match(e.request).then(cachedResponse => {
+      // if yes, then serve files from cache
+      if (cachedResponse) {
+        console.log('Found in cache!');
+        return cachedResponse;
+      }
+      // else do an HTTP request to the server
+      return fetch(e.request);
+    })
+  )
+})
