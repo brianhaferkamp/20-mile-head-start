22e540e5e00793ec8197454c57800718ad7cdbbe
Brian Haferkamp <brian.haferkamp@gmail.com>
2019-07-10 14:03:10 -0500

    Added service worker check

--- source/assets/js/functions.js
+++ source/assets/js/functions.js
@@ -1,3 +1,9 @@
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('') // path to service worker
+    .then(function() { console.log("Service Worker Registered"); });
+}
+
 // This build system utilizes the jQuery library for javascript.
 // Don't want to use jQuery? Just don't load it on the main pages
 // You can also erase this and start writing vanilla JS
